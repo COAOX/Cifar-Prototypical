48f5e327c6a25483f2804cca66e03f73fc858626
COAOX <windowsxu563504792@live.com>
2020-05-15 01:35:31 +0800

    what

--- src/train.py
+++ src/train.py
@@ -211,7 +211,7 @@ def train(opt, model, optim, lr_scheduler):
 
                 loss, acc= loss_fn(model_output, target=y, n_support=opt.num_support_tr, opt=opt, old_prototypes=None if prototypes is None else prototypes,inc_i=inc_i)
 
-                if i == len(tr_dataloader):
+                if i == len(tr_dataloader)-1:
                     pp = compute_prototype(model_output.clone(),y,opt.num_support_tr)
                 loss.backward()
                 print("######next####")
