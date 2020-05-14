a586dcb08d6942f895f90e10324c86219ba4efe7
COAOX <windowsxu563504792@live.com>
2020-05-15 00:30:10 +0800

    what

--- src/train.py
+++ src/train.py
@@ -242,10 +242,10 @@ def train(opt, model, optim, lr_scheduler):
 
         
 
-        if not prototypes is None:
-            prototypes = torch.cat([prototypes,prototype.clone()],dim=0)
-        else:
-            prototypes = prototype.clone()
+#        if not prototypes is None:
+#            prototypes = torch.cat([prototypes,prototype.clone()],dim=0)
+#        else:
+#            prototypes = prototype.clone()
 
         print('Testing with last model..')
         #testf(opt=opt, test_dataloader=test_data, model=model, prototypes=prototypes)
